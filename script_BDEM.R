96e18a4ba1c2dbf0fb6b7ed0023b05bc094555da
jorgerachid1 <jorgerachid@id.uff.br>
2026-09-20 18:32:35 -0300

    script BDEM - SINASC - tarefas 1 a 5

--- script_BDEM.R
+++ script_BDEM.R
@@ -179,6 +179,35 @@ summary(dados_sinasc_2$PESO)
 # KOTELCHUCK = 9 significa "Não informado"   TPROBSON = 11 significa "Não classificado por falta de informação"
 # Em variáveis quantitativas como IDADEMAE verificar se existem valores como 9999 para NA
 
+dados_sinasc_2$LOCNASC[dados_sinasc_2$LOCNASC == 9] = NA
+dados_sinasc_2$ESTCIVMAE[dados_sinasc_2$ESTCIVMAE == 9] = NA
+dados_sinasc_2$GESTACAO[dados_sinasc_2$GESTACAO == 9] = NA
+dados_sinasc_2$GRAVIDEZ[dados_sinasc_2$GRAVIDEZ == 9] = NA
+dados_sinasc_2$PARTO[dados_sinasc_2$PARTO == 9] = NA
+dados_sinasc_2$SEXO[dados_sinasc_2$SEXO == 0] = NA
+dados_sinasc_2$IDANOMAL[dados_sinasc_2$IDANOMAL == 9] = NA
+dados_sinasc_2$ESCMAE2010[dados_sinasc_2$ESCMAE2010 == 9] = NA
+dados_sinasc_2$TPAPRESENT[dados_sinasc_2$TPAPRESENT == 9] = NA
+dados_sinasc_2$TPROBSON[dados_sinasc_2$TPROBSON == 11] = NA
+dados_sinasc_2$KOTELCHUCK[dados_sinasc_2$KOTELCHUCK == 9] = NA
+
+# APGAR5: 99 indica ignorado
+dados_sinasc_2$APGAR5[dados_sinasc_2$APGAR5 == 99] = NA
+
+# conferência após a limpeza
+table(dados_sinasc_2$LOCNASC, useNA = "always")
+table(dados_sinasc_2$ESTCIVMAE, useNA = "always")
+table(dados_sinasc_2$GESTACAO, useNA = "always")
+table(dados_sinasc_2$GRAVIDEZ, useNA = "always")
+table(dados_sinasc_2$PARTO, useNA = "always")
+table(dados_sinasc_2$SEXO, useNA = "always")
+table(dados_sinasc_2$IDANOMAL, useNA = "always")
+table(dados_sinasc_2$ESCMAE2010, useNA = "always")
+table(dados_sinasc_2$TPAPRESENT, useNA = "always")
+table(dados_sinasc_2$TPROBSON, useNA = "always")
+table(dados_sinasc_2$KOTELCHUCK, useNA = "always")
+summary(dados_sinasc_2$APGAR5)
+
 
 # Ao terminar a Tarefa 5 commit com a mensagem "script BDEM - SINASC - tarefas 1 a 5" e envie para o repositório Projeto_BDEM_2016
 
